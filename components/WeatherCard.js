7d3eafa58e7b333bf85048154788e7e416c00c03
saraiesco <saraiescobar@google.com>
2023-10-20 13:38:32 -0500

    dynamic data

--- components/WeatherCard.js
+++ components/WeatherCard.js
@@ -1,12 +1,21 @@
 import React from "react";
-import sunny from "../assets/Sunny.svg"
+import sunny from "../assets/Sunny.svg";
+import cloudy from "../assets/Cloudy.svg";
+import rainy from "../assets/Rainy.svg";
+import partlyCloudy from "../assets/PartlyCloudy.svg";
 
+const images = {
+  "Sunny" : sunny,
+  "Cloudy" : cloudy,
+  "Rainy": rainy,
+  "Partly Cloudy": partlyCloudy
+}
 
 function WeatherCard(props) {
   return (
     <div className = "card">
         <div className = "img-container">
-            <img className="card-img-top" src = {sunny} alt="Card image cap" id = "icon"/>
+            <img className="card-img-top" src = {images[props.data.forecast]} alt="Card image cap" id = "icon"/>
         </div>
         <div class="card-body">
             <h3 className="card-title">{props.data.city}</h3>
